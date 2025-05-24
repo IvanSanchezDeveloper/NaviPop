72a39e4516f3b847a7669ae67b426f5d021676b9
Iván Sánchez Míguez <ivasan07@ucm.es>
2025-05-24 14:32:32 +0200

    Feature | Added google login

--- app/backend/src/Controller/Api/LoginController.php
+++ app/backend/src/Controller/Api/LoginController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Api;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -38,6 +41,46 @@ final class LoginController extends AbstractController
             'user' => [
                 'email' => $user->getEmail(),
                 'roles' => $user->getRoles(),
+            ]
+        ]);
+    }
+
+    #[Route('/api/login/google', name: 'connect_google')]
+    public function connectGoogle(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        return $clientRegistry->getClient('google')->redirect([
+            'email', 'profile'
+        ]);
+    }
+
+    #[Route('/api/login/google/check', name: 'connect_google_validation')]
+    public function connectGoogleCheck(
+        ClientRegistry $clientRegistry,
+        EntityManager $em,
+        JWTTokenManagerInterface $jwtManager
+    ): JsonResponse {
+        $client = $clientRegistry->getClient('google');
+        $googleUser = $client->fetchUser();
+
+        $email = $googleUser->getEmail();
+
+        $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setRoles(['ROLE_USER']);
+            $em->persist($user);
+            $em->flush();
+        }
+
+        $token = $jwtManager->create($user);
+
+        return new JsonResponse([
+            'token' => $token,
+            'user' => [
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles()
             ]
         ]);
     }
