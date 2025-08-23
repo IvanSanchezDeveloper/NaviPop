50981fea63262dc562dbca6e21969d8ddf8ef717
Iván Sánchez Míguez <ivasan07@ucm.es>
2025-08-23 22:50:15 +0200

    Running build-env.sh after DOCKERFILE

--- app/backend/build-env.sh
+++ app/backend/build-env.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 composer install
-echo "Creating and migrating dev database..."
-php bin/console doctrine:database:create --if-not-exists --env=dev
-php bin/console doctrine:migrations:migrate --no-interaction --env=dev
-echo "Creating and migrating test database..."x
-php bin/console doctrine:database:create --if-not-exists --env=test
-php bin/console doctrine:migrations:migrate --no-interaction --env=test
+echo "Creating and migrating database..."
+php bin/console doctrine:database:create --if-not-exists
+php bin/console doctrine:migrations:migrate --no-interaction
 exec symfony serve --allow-http --allow-all-ip --no-tls --port=8000
